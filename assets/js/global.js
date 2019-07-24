cc026281b85c020c4080e522e3bf58d5fdaae9ca
Genarkys <quentin.roussel@genarkys.fr>
2019-07-24 13:21:37 +0200

    [initial]

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,6 +1,9 @@
 const $ = require('jquery');
 
 require ('bootstrap');
+import 'jquery-confirm';
+import 'jquery-confirm/css/jquery-confirm.css'
+
 import '../js/app.js';
 import '../js/add-collection-widget.js';
 
